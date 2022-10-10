830f140be39b1884b6c1b773a66e536e841c11f9
Tribhuwan <mathincosmos@gmail.com>
2022-10-10 13:35:59 +0530

    configure jest with babel

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
 import "./style.css";
 
-function add(x) {
-  return x + 10;
-}
-
-export default add;
--- src/index.test.js
+++
@@ -1,5 +0,0 @@
-import add from "./index";
-
-test("add", () => {
-  expect(add(10)).toBe(20);
-});
