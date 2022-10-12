5798b642e5e493cf9c77b4781dbc9ca67fb811af
Tribhuwan <mathincosmos@gmail.com>
2022-10-12 12:24:37 +0530

    style page header

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import "./style.css";
 
+import iconImg from "./assets/iconShip.png";
+import warIcon from "./assets/war.png";
+
+const icon = document.querySelector("link[rel=icon]");
+icon.href = iconImg;
+const war = document.querySelectorAll("img.warIcon");
+war.forEach((e) => e.src = warIcon);
