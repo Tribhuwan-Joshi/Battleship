2f3fbc9d7d79fd907bebf45ad3d7fb2940ae9089
Tribhuwan <mathincosmos@gmail.com>
2022-10-13 15:24:19 +0530

    add demo battleships

--- src/index.js
+++ src/index.js
@@ -2,8 +2,23 @@ import "./style.css";
 
 import iconImg from "./assets/iconShip.png";
 import warIcon from "./assets/war.png";
+import miyamoto from "./assets/miyamoto.jpg";
 
 const icon = document.querySelector("link[rel=icon]");
 icon.href = iconImg;
+const pfp = document.querySelector("img.pfp");
+pfp.src = miyamoto;
 const war = document.querySelectorAll("img.warIcon");
-war.forEach((e) => e.src = warIcon);
+war.forEach((e) => (e.src = warIcon));
+
+// game board
+const gameBoard = document.querySelector(".gameBoard");
+let n = 100;
+let gameBoardSide = gameBoard.clientHeight;
+let pixSide = gameBoardSide / 10;
+console.log(gameBoardSide, pixSide);
+while (n--) {
+  const pixel = document.createElement("div");
+  pixel.classList.add("border", "border-black");
+  gameBoard.appendChild(pixel);
+}
