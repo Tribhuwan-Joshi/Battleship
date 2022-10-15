51c26e430750f3c855537367ae548fa04779800b
Tribhuwan <mathincosmos@gmail.com>
2022-10-15 18:22:24 +0530

    remove grid shadow on dragleave

--- src/modules/dragdrop.js
+++ src/modules/dragdrop.js
@@ -1,20 +1,31 @@
+const dragDrop = () => {
+  const ships = document.querySelectorAll(".ships");
+  ships.forEach((s) => s.addEventListener("dragstart", dragStart));
+  const pixels = document.querySelectorAll(".gameBoard .pixel");
+  pixels.forEach((p) => {
+    p.addEventListener("dragenter", dragEnter);
+    p.addEventListener("dragover", dragOver);
+    p.addEventListener("dragleave", dragLeave);
+    p.addEventListener("drop", drop);
+  });
 
-const dragDrop = (() => {
-    const ships = document.querySelectorAll(".ships");
-    ships.forEach(s => s.addEventListener('dragstart', dragStart));
-    const pixels = document.querySelectorAll(".gameBoard .pixel");
-    pixels.forEach(p => p.addEventListener("dragenter", dragEnter));
+  function dragOver(e) {
+    e.target.classList.add("drag-over");
+  }
+  function dragEnter(e) {
+    e.target.classList.add("drag-over");
+  }
+  function dragLeave(e) {
+    e.target.classList.remove("drag-over");
+  }
 
-    function dragStart(e) {
-        e.dataTransfer.setData('text/plain', e.target.id);
-        setTimeout(() => e.target.classList.add("hidden"), 0);
-   
-    }
-    function dragEnter(e) {
-        e.target.classList.add("drag-over");
-    }
-
-})
+  function drop(e) {
+    e.target.classList.remove("drag-over");
+  }
+  function dragStart(e) {
+    e.dataTransfer.setData("text/plain", e.target.id);
+    setTimeout(() => e.target.classList.add("hidden"), 0);
+  }
+};
 
 export default dragDrop;
-
